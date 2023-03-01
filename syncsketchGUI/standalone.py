cdbc8977a46ee3c284ff846231ace0c8e093b307
Nathanieljla1 <nathanieljla@gmail.com>
2023-03-01 08:20:23 -0600

    New installer script for python3

--- syncsketchGUI/standalone.py
+++ syncsketchGUI/standalone.py
@@ -1,11 +1,12 @@
 import sys
 import os
+import importlib
 
 main_folder = os.path.dirname(__file__)
 parent_folder = os.path.dirname(main_folder)
 sys.path.append(parent_folder)
 
 import syncsketchGUI
-reload(syncsketchGUI)
+importlib.reload(syncsketchGUI)
 syncsketchGUI.reload_toolkit()
 syncsketchGUI.show_menu_window()
